691337fe3265eef9623295d23f95c2d15343033c
bkarapinar1@hotmail.com <bariskarapinar@bariss-MacBook-Air.local>
2026-04-23 14:26:08 +0300

    Designing AddUpdateDishesActivity

--- app/src/main/java/com/myapp/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/myapp/favdish/view/activities/AddUpdateDishActivity.kt
@@ -3,11 +3,25 @@ package com.myapp.favdish.view.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.myapp.favdish.R
+import com.myapp.favdish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
     }
 
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
 }
