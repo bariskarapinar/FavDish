950b955d0d4e922c61e35bbc25848502bdf7734d
bkarapinar1@hotmail.com <bariskarapinar@bariss-MacBook-Air.local>
2026-04-28 00:36:59 +0300

    RecyclerView and Adapter using ViewBinding

---
+++ app/src/main/java/com/myapp/favdish/view/adapters/CustomListItemAdapter.kt
@@ -0,0 +1,40 @@
+package com.myapp.favdish.view.adapters
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.myapp.favdish.databinding.ItemCustomListLayoutBinding
+
+class CustomListItemAdapter(
+    private val activity: Activity,
+    private val listItems: List<String>,
+    private val selection: String
+) :
+    RecyclerView.Adapter<CustomListItemAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemCustomListLayoutBinding =
+            ItemCustomListLayoutBinding.inflate(LayoutInflater.from(activity), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listItems[position]
+        holder.tvText.text = item
+
+        holder.itemView.setOnClickListener {
+            if (activity is com.myapp.favdish.view.activities.AddUpdateDishActivity) {
+                activity.selectedListItem(item, selection)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+
+    class ViewHolder(view: ItemCustomListLayoutBinding) : RecyclerView.ViewHolder(view.root) {
+        val tvText = view.tvText
+    }
+}
