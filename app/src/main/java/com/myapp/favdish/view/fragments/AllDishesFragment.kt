8429578b01bb09ac87c7aaaf0bc1828368401521
bariskarapinar <bkarapinar1@hotmail.com>
2026-05-14 13:21:29 +0300

    AllDishesFragment.kt Update

--- app/src/main/java/com/myapp/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/myapp/favdish/view/fragments/AllDishesFragment.kt
@@ -3,7 +3,6 @@ package com.myapp.favdish.view.fragments
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
